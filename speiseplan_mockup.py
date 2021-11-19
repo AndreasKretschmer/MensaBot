db720700d24293d10abf701f16ac15fd236e3876
iammteah <immanuel.thoke@gmail.com>
2021-11-19 21:30:31 +0100

    fix variable naming, syntax errors

--- speiseplan_mockup.py
+++ speiseplan_mockup.py
@@ -1,18 +1,18 @@
-Speiseplan = {
+speiseplan = {
     "Standort": "Mensa am Park",
     "Datum": "19-11-2021",
     "Gericht": Gerichte
 }
 
-Gerichte = [{
+gerichte = [{
     "Name": "Pasta Carbonara",
-    "Zutaren": "Makkeroni, Tomatensauce, Gewürze, Gouda"
+    "Zutaten": "Makkeroni, Tomatensauce, Gewürze, Gouda"
     "Preis": "2.00",
-    "Zusatzstoffe": [Zusatzstoffe[0]]
-    "Allergene": [Allergen[0]]
+    "Zusatzstoffe": [Zusatzstoffe[0]],
+    "Allergene": [Allergen[0]],
     "Labels": [Label[1], Label[2], Label[3], Label[4], Label[5]]
 }]
 
-Allergen = ["Milch"]
-Zusatzstoffe = ["Mononatriumglutamat"]
-Label = ["vegan", "vegetarisch", "ohne Schwein", "ohne Fisch", "ohne Alkohol"]
+allergen = ["Milch"]
+zusatzstoffe = ["Mononatriumglutamat"]
+label = ["vegan", "vegetarisch", "ohne Schwein", "ohne Fisch", "ohne Alkohol"]
