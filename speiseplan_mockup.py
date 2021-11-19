165b37fd4f67799da754afd4db243788739501b1
iammteah <immanuel.thoke@gmail.com>
2021-11-19 21:03:13 +0100

    mockup for a Speiseplan

---
+++ speiseplan_mockup.py
@@ -0,0 +1,18 @@
+Speiseplan = {
+    "Standort": "Mensa am Park",
+    "Datum": "19-11-2021",
+    "Gericht": Gerichte
+}
+
+Gerichte = [{
+    "Name": "Pasta Carbonara",
+    "Zutaren": "Makkeroni, Tomatensauce, Gewürze, Gouda"
+    "Preis": "2.00",
+    "Zusatzstoffe": [Zusatzstoffe[0]]
+    "Allergene": [Allergen[0]]
+    "Labels": [Label[1], Label[2], Label[3], Label[4], Label[5]]
+}]
+
+Allergen = ["Milch"]
+Zusatzstoffe = ["Mononatriumglutamat"]
+Label = ["vegan", "vegetarisch", "ohne Schwein", "ohne Fisch", "ohne Alkohol"]
